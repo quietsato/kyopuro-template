5bc5afab04e6812c8829300b0d20ba8113bfec8b
quietsato <quietsato@gmail.com>
2021-02-20 19:53:09 +0900

    Add some libraries

--- Main.hs
+++ Main.hs
@@ -1,16 +1,19 @@
-import Control.Applicative ()
-import Control.Monad ()
-import Data.Array.ST ()
-import Data.STRef ()
+import Control.Applicative
+import Control.Monad
+import Data.Array.ST
+import Data.Functor
+import Data.Maybe
+import Data.STRef
 import qualified Data.Vector as V
 
 -- IO
 getLineAsList :: (Read a) => IO [a]
 getLineAsList = map read . words <$> getLine
 
+-- main
 main :: IO ()
 main = do
-    -- n <- readLn  :: IO Int
-    _ <- getLineAsList :: IO [Int]
+  -- n <- readLn  :: IO Int
+  _ <- getLineAsList :: IO [Int]
 
-    putStrLn "Hello World"
+  putStrLn "Hello World"
