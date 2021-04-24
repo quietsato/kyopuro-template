6960d96185f2d9afb0b74343b2c51b6363a67236
quietsato <quietsato@gmail.com>
2021-04-24 20:47:33 +0900

    Make 'fn main' return Result

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,12 @@
 use itertools::Itertools;
 use whiteread::parse_line;
 
-fn main() {
-    let a: i64 = parse_line().unwrap();
-    let (b, c): (i64, i64) = parse_line().unwrap();
-    let s: String = parse_line().unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let a: i64 = parse_line()?;
+    let (b, c): (i64, i64) = parse_line()?;
+    let s: String = parse_line()?;
 
     println!("{} {}", a + b + c, s);
+
+    Ok(())
 }
