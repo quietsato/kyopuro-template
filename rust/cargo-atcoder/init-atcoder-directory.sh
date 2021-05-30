46336f0a8f5e38f7b75e65420b2a941bcd9c6fb6
quietsato <quietsato@gmail.com>
2021-05-30 23:58:44 +0900

    Use symlic to share snippets

--- rust/cargo-atcoder/init-atcoder-directory.sh
+++ rust/cargo-atcoder/init-atcoder-directory.sh
@@ -6,5 +6,5 @@ echo $ac_dir
 
 mkdir -p $ac_dir/.cargo
 printf '[build]\ntarget-dir = "target"' > $ac_dir/.cargo/config.toml
-printf "#!/bin/bash\ncp -r $(pwd)/../.vscode/ ." > $ac_dir/copy-snippet.sh && chmod +x $ac_dir/copy-snippet.sh
+printf "#!/bin/bash\nln -sf $(pwd)/../.vscode/ ./.vscode" > $ac_dir/copy-snippet.sh && chmod +x $ac_dir/copy-snippet.sh
 
