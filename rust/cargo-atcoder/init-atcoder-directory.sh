8e461b0a64e8d486253bd93f2cd85b6a1b7aa841
quietsato <quietsato@gmail.com>
2021-05-30 20:06:17 +0900

    Improve script `init-atcoder-directory.sh`

--- rust/cargo-atcoder/init-atcoder-directory.sh
+++ rust/cargo-atcoder/init-atcoder-directory.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-mkdir ./.cargo
-printf '[build]\ntarget-dir = "target"' > ./.cargo/config.toml
+ac_dir="$HOME/src/atcoder"
+
+echo $ac_dir
+
+mkdir -p $ac_dir/.cargo
+printf '[build]\ntarget-dir = "target"' > $ac_dir/.cargo/config.toml
+printf "#!/bin/bash\ncp -r $(pwd)/../.vscode/ ." > $ac_dir/copy-snippet.sh && chmod +x $ac_dir/copy-snippet.sh
 
